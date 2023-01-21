b28bef594f237f3f98d2eb16721024182931ddf7
KSoft-Solution <kanhasahu955902@gmail.com>
2023-01-21 18:48:00 +0530

    middlewares setup

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+require("colors");
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { logger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
+const corsOptions = require('./config/corsOption')
+
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 app.all("*", (req, res) => {
@@ -18,6 +29,8 @@ app.all("*", (req, res) => {
   }
 });
 
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
+  console.log(`server is running on http://localhost:${PORT}`.green);
 });
